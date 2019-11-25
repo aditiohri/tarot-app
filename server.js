1d2eedf7c167e49f3bd7ff6ef3c00f956e4659c3
Aditi Ohri <didiohri@gmail.com>
2019-11-25 16:21:31 -0600

    set up dotenv config to atlas

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var methodOverride = require('method-override');
 
+require('dotenv').config();
+require('./config/database');
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
