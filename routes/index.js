27ee9ef3b31f24d3f4709b14532fcbd9f5098491
Aditi Ohri <didiohri@gmail.com>
2019-11-26 17:15:47 -0600

    add OAuth

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,28 @@
 var express = require('express');
 var router = express.Router();
+var passport = require('passport');
 var request = require('request');
 const rootURL = 'https://rws-cards-api.herokuapp.com/api/v1/cards/random';
+
+// Google OAuth login route
+router.get('/auth/google', passport.authenticate('google', {
+  scope: ['profile', 'email']
+}));
+
+// Google OAuth callback route
+router.get('/oauth2callback', passport.authenticate(
+  'google',
+  {
+    successRedirect: '/users',
+    failureRedirect: '/users'
+  }
+));
+
+// OAuth logout route
+router.get('/logout', function(req, res) {
+  req.logout();
+  res.redirect('/users');
+});
 
 /* GET home page. */
 // router.get('/', function(req, res, next) {
@@ -18,6 +39,7 @@ router.get('/', function(req, res) {
     res.render('index', {
       title: 'Tarot Reading',
       cards: body,
+      user: req.user
     })
   });
 });
